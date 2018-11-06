08783c33e23a193369386aca2ebd33f87bd93386
David Dormagen <czapper@gmx.de>
2018-11-06 15:30:10 +0100

    initial

---
+++ clone_repositories.sh
@@ -0,0 +1,8 @@
+# This helper repository expects the top-level directory to contain sub-directories with the exact same name as the github repositories.
+# For example, as cloned by this script.
+git clone git@github.com:BioroboticsLab/behavior_loader.git
+git clone git@github.com:BioroboticsLab/biotracker_interfaces.git
+git clone git@github.com:BioroboticsLab/biotracker_utility.git
+git clone git@github.com:BioroboticsLab/biotracker_core.git
+git clone git@github.com:BioroboticsLab/biotracker_backgroundsubtraction_tracker.git
+git clone git@github.com:BioroboticsLab/biotracker_lucasKanade.git
